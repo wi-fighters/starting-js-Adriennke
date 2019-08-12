0bf9225db0963375184b9ca91b253885b4ac3a49
Adrienn <adrienntollas@gmail.com>
2019-08-12 19:27:22 +0200

    javascript-exercises

--- getstarted-1/index.js
+++ getstarted-1/index.js
@@ -18,3 +18,29 @@ let last = "Johnson"; //string
 let car = "trabant";  //string
 let isOpen = true; //boolean
 
+var ageMark = "32"
+var ageJohn =  45 
+console.log("32",45)
+console.log("32","45")
+
+var John = 45;
+var Mark = 32;
+console.log("John"+" "+"is"+" "+"older"+" "+"than"+" "+"Mark");
+console.log(John>Mark);
+
+var a = 11;
+var b = 12;
+var c = 13;
+console.log(a===b===c);
+console.log(a==b);
+console.log(b==c);
+
+var programmingLanguage = "JavaScript";
+var isFun = true;
+console.log("JavaScript"+" "+"is"+" "+"fun:"+" "+"true");
+
+var test
+console.log("string");
+
+var calculation = 4+4;
+console.log(calculation);
