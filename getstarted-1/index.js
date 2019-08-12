ea305711c1dab3bb610b42cf9f770c199f9e524c
Adrienn <adrienntollas@gmail.com>
2019-08-12 16:17:12 +0200

    getstarted

---
+++ getstarted-1/index.js
@@ -0,0 +1,20 @@
+var firstName;
+firstName = "Adrienn";
+var lastName;
+lastName = "Tollas";
+console.log(firstName,lastName);
+
+var name = "Laura";
+var city = "Berlin";
+var job = "carpenter";
+var age = "45";
+var family = "relationship"
+console.log(name,city,job,age,family)
+
+console.log("John" + " " + "Smith" + " " + "is" + " " + "a" + " " + "43" + " "+ "year" + " " + "old" + " " + "teacher" + " " + "who" + " " + "lives" + " " + "in" + " " + "Berlin")
+
+let length = 16.78; //number
+let last = "Johnson"; //string
+let car = "trabant";  //string
+let isOpen = true; //boolean
+
