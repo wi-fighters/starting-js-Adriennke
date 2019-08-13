f10dde572bb28038d305f65eeeb44493ec3664bc
Adrienn <adrienntollas@gmail.com>
2019-08-13 16:11:58 +0200

    first-one

---
+++ arithmetic/index.js
@@ -0,0 +1,57 @@
+let x = (17 / 30) * 100;
+console.log(x + "%");
+
+let a = 8
+let b = 6
+let c = 5
+let d = 9
+let e = 8
+let f = 2
+let g = 1
+let h = 8.5
+let i = 7
+let k = 4
+let average = (a+b+c+d+e+f+g+h+i+k)
+let result = (average / 10);
+console.log(result);
+
+let z = (85*6)-(75+70+85+90+100)
+console.log(`Score in the sixth test:${z}`);
+
+let totalBeforeFinalExam = 8*78
+let totalNumberOfExam = 9
+let goalAverage = 80
+let goalForFinalExam =(totalNumberOfExam*goalAverage)-(totalBeforeFinalExam)
+console.log(`James needs a minimum of ${goalForFinalExam}% to get an 80% average.`);
+
+let tip1 = 22.35 * 0.1
+let tip2 = 26.67 * 0.15
+let tip3 = 35.92 * 0.2
+let totalBill = (22.35 + tip1)
+let totalBill2 = (26.67 + tip2)
+let totalBill3 = (35.92 + tip3)
+console.log(`${totalBill} ${totalBill2} ${totalBill3}`);
+
+let perimeterSquare = 4.75*4
+console.log(`${perimeterSquare}`);
+
+
+let perimeterTriangle = 5 + 6 + 7
+console.log(`${perimeterTriangle}`);
+
+let areaSquare = 5*5
+console.log(`${areaSquare}`);
+
+let areaTriangle = (5 + 6 + 7)/2
+console.log(`${areaTriangle}`);
+
+let volumeOfCube = 9*9*9
+console.log(`${volumeOfCube}`);
+
+let ticket = 6450 / 15
+console.log(`${ticket} people purchased a ticket.`);
+
+
+let yearlyIncome = 500 * 52
+console.log(`${yearlyIncome}`);
+
