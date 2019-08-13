a542fd2af6c7ea5beda034dc5d082fd703b9b4ce
Adrienn <adrienntollas@gmail.com>
2019-08-13 10:50:08 +0200

    update

--- getstarted-1/index.js
+++ getstarted-1/index.js
@@ -9,24 +9,28 @@ var city = "Berlin";
 var job = "carpenter";
 var age = "45";
 var family = "relationship"
-console.log(name,city,job,age,family)
+console.log(name + " is a " + job + " living in " + city + " she is " + age + " and in a " + family);
 
-console.log("John" + " " + "Smith" + " " + "is" + " " + "a" + " " + "43" + " "+ "year" + " " + "old" + " " + "teacher" + " " + "who" + " " + "lives" + " " + "in" + " " + "Berlin")
+console.log("John"+ " Smith"+ " is"+ " a" + " 43 "+ "year " + "old "+ "teacher "+ "who" +" lives "+ "in" +" Berlin")
 
 let length = 16.78; //number
 let last = "Johnson"; //string
 let car = "trabant";  //string
 let isOpen = true; //boolean
+console.log(typeof length);
+console.log(typeof last);
+console.log(typeof car);
+console.log(typeof isOpen);
 
 var ageMark = "32"
 var ageJohn =  45 
-console.log("32",45)
-console.log("32","45")
+console.log(ageMark);
+console.log(ageJohn);
 
 var John = 45;
 var Mark = 32;
-console.log("John"+" "+"is"+" "+"older"+" "+"than"+" "+"Mark");
-console.log(John>Mark);
+console.log("John "+"is "+"older "+"than "+"Mark", John>Mark);
+
 
 var a = 11;
 var b = 12;
@@ -34,13 +38,20 @@ var c = 13;
 console.log(a===b===c);
 console.log(a==b);
 console.log(b==c);
+console.log(a===c);
+console.log(typeof (a===c));
 
 var programmingLanguage = "JavaScript";
 var isFun = true;
-console.log("JavaScript"+" "+"is"+" "+"fun:"+" "+"true");
+console.log(programmingLanguage + " is" + " fun:" + isFun);
 
 var test
-console.log("string");
+console.log(typeof test);
 
 var calculation = 4+4;
 console.log(calculation);
+
+
+var a = 8573254
+var b = 536
+console.log(a*b);
