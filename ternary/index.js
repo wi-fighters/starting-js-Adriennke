5ddc74fd179e5fca0c4bf6f1d238b68b7ad89f82
Adrienn <adrienntollas@gmail.com>
2019-08-14 11:01:36 +0200

    ternary file

---
+++ ternary/index.js
@@ -0,0 +1,28 @@
+const legalAge = 18
+const personsAge = 16
+const canDrive = (personsAge>=legalAge) ? "Yes, you can drive! Hope you have a driver license." : "Nope, you´re too young!"
+console.log(canDrive);
+
+
+const speedLimit = 130
+const actualSpeed = 120
+const speedCheck = (speedLimit<=actualSpeed) ? "You are going too fast! Slow down!" : "You are driving below the speed limit."
+console.log(speedCheck);
+
+const speedCheck2 = (speedLimit>=actualSpeed) ? "You are going too fast! Slow down!" : "You are driving below the speed limit."
+console.log(speedCheck2);
+
+const tooYoung = 19
+const oldEnough = 21
+const personAge = (tooYoung<=oldEnough)? "Here is some juice" : "Here is some wine."
+console.log(personAge);
+
+const itIsStudent = false
+const isStudent = (itIsStudent)? "Ticket costs €5,00":"Ticket costs €12,00"
+console.log(isStudent);
+
+const morning = true
+const timeOfDay = (morning) ? "Good Morning":"Good Evening"
+console.log(timeOfDay);
+
+ 
