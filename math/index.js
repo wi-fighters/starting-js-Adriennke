e121bfa1d4c7a4c7aea60420af05d8707336107e
Adrienn <adrienntollas@gmail.com>
2019-08-19 15:47:06 +0200

    math

---
+++ math/index.js
@@ -0,0 +1,34 @@
+//Lowest number
+
+console.log(Math.min(45,768,38752845,0,758,221));
+
+//Highest number
+
+console.log(Math.max(76,847,9303,92928,463829221));
+
+//Rounding
+
+//Round up
+
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+
+
+//Round down
+
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+
+//Dice Roll
+
+console.log(Math.random()+1);
+console.log(Math.ceil(1.74243611652123));
+
+
+
