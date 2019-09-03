5c8c9d4055c82b54af24e9d2d7716eaa8e44e959
Adrienn <adrienntollas@gmail.com>
2019-09-03 14:52:29 +0200

    no idea

--- übungen/üben.js
+++ übungen/üben.js
@@ -83,3 +83,15 @@ switch (val) {
     break;
   default: 7
     answer = ("Ate Nine");
+
+
+    function abTest(a, b) {
+      // Only change code below this line
+      if (a < 0 || b < 0) {
+      return undefined;
+      }
+    }
+  
+    abTest(2, 2);
+
+    console.log(abTest());
